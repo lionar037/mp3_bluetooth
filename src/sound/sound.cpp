84cae63fab442c02d296483acb7ea317792e0f1b
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-17 07:45:06 +0100

    202410170745 .

--- src/sound/sound.cpp
+++ src/sound/sound.cpp
@@ -10,34 +10,34 @@ namespace SOUND_LIST{
         sound_list();
     }
 
-int SoundList_t::sound_list() {
-    std::string folderPath = "assets/sounds"; // Cambia esto por la ruta a tu carpeta
-    std::vector<std::string> mp3Files;
-
-    try {
-        // Iterar sobre los archivos en la carpeta
-        for (const auto& entry : fs::directory_iterator(folderPath)) {
-            // Verificar si la extensión es .mp3 (case insensitive)
-            if (entry.path().extension() == ".mp3") {
-                mp3Files.push_back(entry.path().string());
-                mp3_list.push_back(entry.path().string());
+    int SoundList_t::sound_list() {
+        std::string folderPath = "assets/sounds"; // Cambia esto por la ruta a tu carpeta
+        std::vector<std::string> mp3Files;
+    
+        try {
+            // Iterar sobre los archivos en la carpeta
+            for (const auto& entry : fs::directory_iterator(folderPath)) {
+                // Verificar si la extensión es .mp3 (case insensitive)
+                if (entry.path().extension() == ".mp3") {
+                    mp3Files.push_back(entry.path().string());
+                    mp3_list.push_back(entry.path().string());
+                }
             }
+        } catch (const fs::filesystem_error& e) {
+            std::cerr << "Error al leer la carpeta: " << e.what() << std::endl;
+            return -1;
         }
-    } catch (const fs::filesystem_error& e) {
-        std::cerr << "Error al leer la carpeta: " << e.what() << std::endl;
-        return -1;
+    
+        // Mostrar la lista de archivos MP3 encontrados
+        std::cout << "MP3 encontrados en la carpeta:\n";
+        for (const auto& file : mp3Files) {
+            std::cout << file << std::endl;
+        }
+    
+        return 0;
     }
 
-    // Mostrar la lista de archivos MP3 encontrados
-    std::cout << "MP3 encontrados en la carpeta:\n";
-    for (const auto& file : mp3Files) {
-        std::cout << file << std::endl;
-    }
-
-    return 0;
-}
-
- const std::vector <std::string>& SoundList_t::convertToMayuscule(std::vector<std::string>& display_text ){
+    const std::vector <std::string>& SoundList_t::convertToMayuscule(std::vector<std::string>& display_text ){
 	for (auto& str : display_text) 
     std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) { return std::toupper(c); });  // Convertir cada cadena a mayúsculas
     return display_text;    
